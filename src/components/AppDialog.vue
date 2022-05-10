5c02ea914d52fa745c5bd05d1b50dab6e4709120
amberlin10706 <yyyy2248@hotmail.com>
2022-05-10 14:47:07 +0800

    ans1

---
+++ src/components/AppDialog.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="dialog-cover" v-if="isShow">
+    <div class="dialog">
+      <div class="dialog_close" @click="closeDialog">x</div>
+      <div class="dialog_title">
+        <slot name="title">標題</slot>
+      </div>
+      <div class="dialog_content">
+        <slot name="content">內文</slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppDialog",
+  props: ["isShow"],
+  emits: ["close"],
+  methods: {
+    closeDialog() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dialog {
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #2e2c2d;
+  font-size: 16px;
+  background-color: white;
+  width: 300px;
+  height: 300px;
+}
+.dialog_close {
+  display: flex;
+  justify-content: end;
+  font-size: 28px;
+  padding-right: 8px;
+}
+.dialog-cover {
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="open">open</button>
+    <div>count {{ count }}</div>
+    <AppDialog :is-show="isShow" @close="close">
+      <template v-slot:title>title</template>
+      <template v-slot:content>
+        <button @click="add">+</button>
+      </template>
+    </AppDialog>
   </div>
 </template>
+
+<script>
+import AppDialog from "@/components/AppDialog.vue";
+import store from "@/store";
+
+export default {
+  name: "Demo",
+  components: {
+    AppDialog,
+  },
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+  computed: {
+    count() {
+      return store.state.count;
+    },
+  },
+  methods: {
+    open() {
+      this.isShow = true;
+    },
+    close() {
+      this.isShow = false;
+    },
+    add() {
+      store.dispatch("increment");
+    },
+  },
+};
+</script>
